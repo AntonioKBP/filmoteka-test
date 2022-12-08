3f752742f7c726a53d6dcafdd74b075b266d274f
AntonioKBP <105855258+AntonioKBP@users.noreply.github.com>
2022-12-08 19:39:18 +0000

    Deploying to gh-pages from @ AntonioKBP/filmoteka-test@2fb1a1b00bcb27976cadcd84c9a95a70bd31bbe6 🚀

---
+++ index.8349c674.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("myModal"),t=document.querySelector(".rate-widget-open-btn-icon"),n=document.querySelector(".rate-widget__btn");t.onclick=function(){e.style.display="block"},window.onclick=function(t){t.target===e&&(e.style.display="none")},n.addEventListener("click",(function(e){e.preventDefault()}));
+//# sourceMappingURL=index.8349c674.js.map
---
+++ index.c6b823e0.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("myModal"),t=document.querySelector(".rate-widget-open-btn-icon"),n=document.querySelector(".rate-widget__btn");t.onclick=function(){e.style.display="block"},window.onclick=function(t){t.target===e&&(e.style.display="none")},n.addEventListener("click",(function(e){e.preventDefault()}))}();
+//# sourceMappingURL=index.c6b823e0.js.map
