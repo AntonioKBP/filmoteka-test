63b90e3fa4834f883ef56a0799b9b868405d78b4
Anton Kovsh <kovsh_anton@ukr.net>
2022-12-08 19:22:04 +0200

    upd

--- src/js/rate-widget.js
+++ src/js/rate-widget.js
@@ -15,20 +15,20 @@
 
 const modal = document.getElementById('myModal');
 
-const btn = document.getElementById('myBtn');
-
-const span = document.getElementsByClassName('close')[0];
-
+const btn = document.querySelector('.rate-widget-open-btn-icon');
+const submitBtn = document.querySelector('.rate-widget__btn');
 btn.onclick = function () {
   modal.style.display = 'block';
 };
 
-span.onclick = function () {
-  modal.style.display = 'none';
-};
-
 window.onclick = function (event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = 'none';
   }
 };
+
+submitBtn.addEventListener('click', onCloseModal);
+
+function onCloseModal(e) {
+  e.preventDefault();
+}
